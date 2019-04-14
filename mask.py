23ed79610f9207321903207f292c64812b01ee96
hama6767 <hama6767@iis.u-tokyo.ac.jp>
2019-04-14 17:19:08 +0900

    Add liner-square method

--- mask.py
+++ mask.py
@@ -8,7 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 distance_of_array = 200 # mm
-sensor_size = 17.3 # y方向
+sensor_size = 19.2 # y方向
+
 
 
 
@@ -20,7 +21,11 @@ def mask():
   before_x = 0
   before_y = 0
   error_count = 1
-
+  mcn = -1
+  pixel_height = 720
+  pixel_width = 1280
+  angle_of_view = 62.2 * 3.1415 / 180
+  sensor_size = 37.7 * 3.1415 / 180
   #カメラ補正
   camera_matrix = np.array([[1.06105005e+003, 0.000000, 6.48830933e+002], [0.000000, 1.06279529e+003, 3.83095215e+002], [0., 0., 1.]])
   distortion_params = np.array([-2.45146647e-001, 1.50185645e-001, 2.24484666e-003, -2.79057189e-003, 0.000000])
@@ -31,10 +36,10 @@ def mask():
   # img = cv2.imread('c:\Users\makilab\Desktop\Docking-master\Docking-master\sample.jpg') # Errorのため絶対パスで指定　要改善
 
 
-  for i in range(50):
+  for i in range(55):
     img = cv2.imread('c:\Users\makilab\Desktop\Docking-master\Docking-master\images\img{0:05d}.jpg'.format(i),)
     
-    
+    mcn = mcn + 1
     img_undistort = cv2.remap(img, map_func[0], map_func[1], cv2.INTER_AREA)
 
 
@@ -74,12 +79,16 @@ def mask():
     cx = []
     cy = []
     area = []
+    #Tw = 0.0036736 / 640
+    #Th = 0.0027384 / 360
+    Tw = ( 2 * np.tan(angle_of_view/2)) / pixel_width * (-1)
+    Th = ( 2 * np.tan(sensor_size/2)) / pixel_height
     for i in range(len(large_contours)):
       cnt = large_contours[i]
       M = cv2.moments(cnt)
       # print M['m00']
-      cx.append(int(M['m10']/M['m00']))
-      cy.append(int(M['m01']/M['m00']))
+      cx.append((int(M['m10']/M['m00']) - pixel_width/2) * Tw )
+      cy.append((int(M['m01']/M['m00']) - pixel_height/2) * Th)
       area.append(int(M['m00']))
       # print(cx[i], cy[i])
 
@@ -88,47 +97,50 @@ def mask():
     top_area_number = np.argmin(cy)
     right_area_number = np.argmax(cx)
     left_area_number = np.argmin(cx)
-    
+
     L1 =  3.762 # 0.26978 # ヨコ
     L2 = 2.975 # 0.22189 # タテ
-    a = 0.1500
+    h = 0.075
     W = 43.00 #58
     W2 = 30.00 # 720.000
-    h1 = cx[right_area_number] - cx [left_area_number] # 画像上の左右長さ px
-    h2 = (cy[right_area_number] + cy[left_area_number]) / 2 - cy[top_area_number]
+
+    
+    
+    oy2 = cx[top_area_number] 
+    oz2 = cy[left_area_number] + (cy[right_area_number] - cy[left_area_number]) * (oy2 - cx[left_area_number]) / (cx[right_area_number] - cx[left_area_number])
+    ox =  h / (oz2 - cy[top_area_number]) 
+    theta = np.arctan(oy2)
+    r = ox * np.sqrt(1 + oy2 * oy2)
+    oz = ox * oz2
+    oy = np.sin(theta) * r
+    theta2 = np.arctan(cx[largest_area_number]) - theta
+    sin_alpha_1 = ((2*r / (h * np.tan(theta2))) - np.sqrt((2*r / (h * np.tan(theta2)))**2 - 4 * (1 + 1/((np.tan(theta2))**2)) * (r*r/h/h - 1)))/ (2 *  (1 + 1/((np.tan(theta2))**2)))
+    sin_alpha_2 = ((2*r / (h * np.tan(theta2))) + np.sqrt((2*r / (h * np.tan(theta2)))**2 - 4 * (1 + 1/((np.tan(theta2))**2)) * (r*r/h/h - 1)))/ (2 *  (1 + 1/((np.tan(theta2))**2)))
+    D = (2*r / (h * np.tan(theta2)))**2 - 4 * (1 + 1/((np.tan(theta2))**2)) * (r*r/h/h - 1)
+
+    alpha2 = np.arcsin(sin_alpha_1)
+    alpha = theta + 3.14 - alpha2
+    print ox,r
 
 
-    D1 = L1 * (W / float(h1))
-    D2 = L2 * (W2 / float(h2))
-    print D1
+   
 
     # 向き計算
-    pixel_width = 1280
-    pixel_height = 720
-    angle_of_view = 31.07
+    
+    
 
     relative_angle = ( (cx[largest_area_number] * angle_of_view / pixel_width) - angle_of_view / 2 ) * 3.1415 / 180 # 相対角を計算
 
     #print relative_angle
 
     # 座標を出力
-    x = np.cos(3.1415 / 2 - relative_angle) * D1
-    y = np.sin(3.1415 / 2 - relative_angle) * D1
-    print x,y
+
 
 
     Hz = 1.25 * error_count # logより逆算　ROSでは実際の稼働Hzを入れる
 
-    velocity_x = (x - before_x) / Hz
-    velocity_y = (y - before_y) / Hz
-    velocity_distance = np.sqrt(velocity_x **2 + velocity_y**2)
-
-    print velocity_x,velocity_y,velocity_distance
-
-    before_x = x
-    before_y = y
-    csvlist.append(velocity_y)
     error_count = 1
+    
 
   # 出力
   writer.writerow(csvlist)
